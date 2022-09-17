0dce14306750f826d33c0a1a3dc410ed6bfdd9d3
Lawrence C <lawrencecaudle870@gmail.com>
2022-09-16 23:12:55 -0400

    added logo and text to Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Header() {
+    return (
+        <header className="header">
+            <img className="header-logo" src="/src/images/earth-africa-solid.svg" alt="" />
+            <p className="header-text">my travel journal</p>
+        </header>
+    )
+}
