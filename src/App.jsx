c1dbd51feb2b3b51a834ab71d55e0497c3705c24
Lawrence C <lawrencecaudle870@gmail.com>
2022-09-17 00:17:31 -0400

    added Main component to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
-import { useState } from 'react'
-import Header from './components/Header'
-import './css/App.css'
+import { useState } from "react";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import data from "./data.js";
+import "./css/App.css";
 
 function App() {
-  return (
-    <div className='container'>
-      <Header />
-    </div>
-  )
+    const info = data.map((detail) => {
+        return (
+            <Main
+                key={detail.id}
+                title={detail.title}
+                location={detail.location}
+                map={detail.googleMapsUrl}
+                startDate={detail.startDate}
+                endDate={detail.endDate}
+                description={detail.description}
+                image={detail.imageUrl}
+            />
+        );
+    });
+
+    return (
+        <div className="container">
+            <Header />
+            <section>{info}</section>
+        </div>
+    );
 }
 
-export default App
+export default App;
