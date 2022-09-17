a9a49dc0b8368b6541d000040f2cfe188597f855
Lawrence C <lawrencecaudle870@gmail.com>
2022-09-16 23:12:09 -0400

    added Header component to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
+import Header from './components/Header'
 import './css/App.css'
 
 function App() {
   return (
     <div>
-      
+      <Header />
     </div>
   )
 }
